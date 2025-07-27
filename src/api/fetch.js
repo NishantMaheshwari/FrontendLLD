cb7ba2e714b650ca0eea5bbb0d135c8c4d89548a
Nishant Maheshwari <nishant.maheswari@knolskape.com>
2025-07-27 17:12:50 +0530

    added tailwind

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,6 +1,18 @@
-const fetch = async (url, options) => {
-  const response = await fetch(url, options);
-  return response.json();
+const callAPI = async (url, method = 'POST', payload = {}) => {
+  const response = await fetch(url, {
+    method,
+    body: JSON.stringify(payload),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  });
+  if(!response.ok){
+    const errorData = await response.json();
+    throw new Error(errorData.error?.message || 'API Error');
+  }
+  return await response.json();
 };
 
-export default fetch;
+export const shimmerData = async () => {
+  const data = await callAPI('','GET');
+}
---
+++ src/pages/Shimmer/ShimmerUI.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { shimmerData } from '../../api/fetch'
+
+const ShimmerUI = () => {
+  return (
+    <div className=''>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default ShimmerUI;
