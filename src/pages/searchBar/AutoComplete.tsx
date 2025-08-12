8e31d79d8aa8ec73fe8bb8ad9fc953fbb4f3690a
Nishant Maheshwari <nishant.maheswari@knolskape.com>
2025-08-12 18:41:52 +0530

    added clicking on suggestion behaviour for autocomplete

--- src/pages/searchBar/AutoComplete.tsx
+++ src/pages/searchBar/AutoComplete.tsx
@@ -37,12 +37,16 @@ const AutoComplete = (): ReactElement => {
     cache.current.set(searchQuery, results);
     setSuggestions(results);
   }
-  console.log(suggestions);
+
+  const handleSelectingSuggestion = (name: string) => {
+    setSearchQuery(name);
+  }
+
   return (
     <div className='flex justify-center mt-20'>
       <div className='w-[40vw] h-[3vw] border-1 border-gray-950'>
-        <div className='flex py-1.5 px-2 mb-1'>
-          <input className='w-[90%] h-full focus:border-transparent focus:outline-none'
+        <div className='flex h-full'>
+          <input className='w-[90%] h-full focus:border-transparent focus:outline-none py-1.5 px-2'
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             onFocus={() => setShowSuggestions(true)}
@@ -50,19 +54,24 @@ const AutoComplete = (): ReactElement => {
             type='text'
             placeholder='Type here...'>
           </input>
-          <button className='w-[10%] h-full'>
-
+          <button className='w-[10%] h-full bg-blue-600 text-white cursor-pointer'
+            onClick={() => setSearchQuery('')}>
+            Clear
           </button>
         </div>
-        <div className='w-full rounded-b-2xl border-1 border-l-black border-r-black border-b-black'>
-          {
-            searchQuery && showSuggestions && suggestions.map((suggestion, index) => (
-              <div key={suggestion.id} className={`flex w-full h-[2vw] px-2 py-1 ${(index < suggestions.length - 1) ? 'border-b-gray-500 border-b-1' : ''} cursor-pointer hover:bg-blue-400`}>
-                {suggestion.name}
-              </div>
-            ))
-          }
-        </div>
+        {
+          searchQuery && showSuggestions &&
+          <div className='w-full rounded-b-2xl border-1 border-l-black border-r-black border-b-black'>
+            {
+              suggestions.map((suggestion, index) => (
+                <div key={suggestion.id} className={`flex w-full h-[2vw] px-2 py-1 ${(index < suggestions.length - 1) ? 'border-b-gray-500 border-b' : ''} cursor-pointer hover:bg-blue-400`}
+                  onMouseDown={() => handleSelectingSuggestion(suggestion.name)}>
+                  {suggestion.name}
+                </div>
+              ))
+            }
+          </div>
+        }
       </div>
     </div>
   )
